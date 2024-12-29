8756d89b3bd4ae8e0705a137f96d4ef46dbb3623
Umesh Kumar <127929058+Vargos98@users.noreply.github.com>
2024-12-29 16:39:13 +0530

    Update
    implemented JWT and bcrypt hashing also added the respective secret in dotenv file

--- src/models/video.model.js
+++ src/models/video.model.js
@@ -1,45 +1,51 @@
-import mongoose,{Schema} from "mongoose";
-import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2";
+import mongoose, { Schema } from "mongoose"; 
+// Import mongoose and Schema class to define the data structure for the MongoDB collection
+
+import mongooseAggregatePaginate from "mongoose-aggregate-paginate-v2"; 
+// Import the mongoose plugin to add pagination functionality for aggregation queries
+
 const videoSchema = new Schema(
   {
-    videoFile:{
-      type:String, // cloudinary url
-      required:true,
-
+    videoFile: {
+      type: String, // cloudinary url
+      required: true, // Mandatory field
     },
-    thumbnail:{
-      type:String, // cloudinary url
-      required:true,
+    thumbnail: {
+      type: String, // cloudinary url
+      required: true, // Mandatory field
     },
-    title:{
-      type:String, 
-      required:true,
+    title: {
+      type: String, // Title of the video
+      required: true, // Mandatory field
     },
-    description:{
-      type:String, 
-      required:true,
+    description: {
+      type: String, // Description of the video
+      required: true, // Mandatory field
     },
-    duration:{
-      type:Number, 
-      required:true,
+    duration: {
+      type: Number, // Duration of the video in seconds
+      required: true, // Mandatory field
     },
-    views:{
-      type:Number,
-      default:0,
+    views: {
+      type: Number, // Number of views the video has received
+      default: 0, // Default value is 0
     },
-    isPublished:{
-      type:Boolean,
-      default:true,
+    isPublished: {
+      type: Boolean, // Boolean to indicate whether the video is published
+      default: true, // Default value is true
     },
-    owner:{
-      type: Schema.Types.ObjectId,
-      ref:'User',
-      required:true,
-    }
-
+    owner: {
+      type: Schema.Types.ObjectId, // Reference to the User collection
+      ref: 'User', // Specifies the referenced model
+      required: true, // Mandatory field
+    },
   },
-  {timestamps:true}); 
+  { timestamps: true } 
+  // Adds `createdAt` and `updatedAt` fields to the schema automatically
+);
 
-  videoSchema.plugin(mongooseAggregatePaginate); 
+// Add the mongooseAggregatePaginate plugin to the schema
+videoSchema.plugin(mongooseAggregatePaginate); 
 
-  export const Video = mongoose.model('Video',videoSchema);
+// Export the Video model based on the schema
+export const Video = mongoose.model('Video', videoSchema); 
